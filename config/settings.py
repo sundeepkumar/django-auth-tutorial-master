85a29168c141871e3989417fe03267b16ad00e2d
sundeep@sundeepkumar.com <sundeep@sundeepkumar.com>
2021-11-10 18:04:09 -0500

    third commit

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import dj_database_url
+#import dj_database_url
 #import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
